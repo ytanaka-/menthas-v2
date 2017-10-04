36d3265288bc7f11b4230cda3d8d7ad99e1edd0a
ytanaka <y.matsuura000@gmail.com>
2017-10-05 02:13:41 +0900

    remove unneeded code

--- config.default.js
+++ config.default.js
@@ -13,10 +13,6 @@ module.exports = {
     // This is the id of the project you created in Google Cloud.
     // e.g. https://console.developers.google.com/project/<projectId>
     projectId: 'project-id-here',
-    // Path to the JSON key file you downloaded when you created the Service Account
-    // credentials for your Google Cloud project.
-    // See https://console.developers.google.com/project/<projectId>/apiui/credential
-    keyFile: './key.json',
     // Datastore Namespace
     namespace: ''
   },
